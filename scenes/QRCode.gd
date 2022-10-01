5625d7bc3902e336546c7a1396275b785bea2649
Matthew Campbell <hyper@hyperworks.nu>
2022-10-02 01:13:55 +0700

    QR code generation code

--- scenes/QRCode.gd
+++ scenes/QRCode.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+@onready var text_label := $url_label
+@onready var qr_code_image := $QrCode
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var qr_code: QrCode = QrCode.new()
+	var url1 = "http://localhost:8001/demo url"
+	var texture: ImageTexture = qr_code.get_texture(url1)
+
+	text_label.text = "Client url : " + url1
+	qr_code_image.texture = texture
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scenes/main.gd
+++ scenes/main.gd
@@ -8,7 +8,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
 
 
-func _on_button_pressed():
+func _on_song_select_btn_pressed():
+	get_tree().change_scene_to_file("res://scenes/SongSelect.tscn")
+
+
+func _on_qr_code_btn_pressed():
 	get_tree().change_scene_to_file("res://scenes/QRCode.tscn")
+
+
+func _on_options_btn_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_btn_pressed():
+	pass # Replace with function body.
